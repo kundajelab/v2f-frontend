a31008b3bb89b83d9804512f11a178f47300696e
Riya Sinha <riya.v.sinha@gmail.com>
2023-07-11 12:47:50 -0500

    feat: add fragment to fetch bpnet prediction data in variant page

---
+++ src/components/BpnetPredictionTable.tsx
@@ -0,0 +1,115 @@
+import { OtTable, Link, Tooltip } from '../ot-ui-components';
+
+import { VariantPageAbcPredictionFragment } from '../__generated__/graphql';
+import { ApolloError } from '@apollo/client';
+import { HourglassTop } from '@mui/icons-material';
+
+type TableColumn<T> = {
+  id: string;
+  label: string;
+  tooltip?: React.ReactNode;
+  renderCell?: (rowData: T) => React.ReactNode;
+};
+
+const tableColumns = (
+  _variantId: string
+): TableColumn<VariantPageAbcPredictionFragment>[] => [
+  {
+    id: 'isTemporary',
+    label: '',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) =>
+      rowData.isTemporary ? (
+        <Tooltip title="Temporary prediction uploaded by a user">
+          <HourglassTop
+            sx={{ opacity: 0.5, fontSize: '20px', verticalAlign: 'middle' }}
+          />
+        </Tooltip>
+      ) : null,
+  },
+  {
+    id: 'cellType',
+    label: 'Cell Type',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) => rowData.cellType,
+  },
+  {
+    id: 'targetGene',
+    label: 'Target Gene',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) => (
+      <Link key={rowData.targetGene.id} to={`/gene/${rowData.targetGene.id}`}>
+        {rowData.targetGene.symbol}
+      </Link>
+    ),
+  },
+  {
+    id: 'abcScore',
+    label: 'ABC Score',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) => rowData.score,
+  },
+  {
+    id: 'dataset',
+    label: 'Dataset',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) => rowData.dataset,
+  },
+  {
+    id: 'model',
+    label: 'Model',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) => rowData.model,
+  },
+  {
+    id: 'variantGeneDistance',
+    label: 'Variant-Gene Distance',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) =>
+      rowData.variantToGeneDistance,
+  },
+  {
+    id: 'enhancerStart',
+    label: 'Enhancer Start',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) =>
+      rowData.enhancerStart,
+  },
+  {
+    id: 'enhanerEnd',
+    label: 'Enhancer End',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) =>
+      rowData.enhancerEnd,
+  },
+  {
+    id: 'enhancerClass',
+    label: 'Enhancer Class',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) =>
+      rowData.enhancerClass,
+  },
+  {
+    id: 'targetGenePromoterActivityQuantile',
+    label: 'Target Gene Promoter Activity Quantile',
+    renderCell: (rowData: VariantPageAbcPredictionFragment) =>
+      rowData.targetGenePromoterActivityQuantile,
+  },
+];
+
+type AbcPredictionsTableProps = {
+  loading: boolean;
+  error?: ApolloError;
+  filenameStem: string;
+  data: VariantPageAbcPredictionFragment[];
+  variantId: string;
+};
+const AbcPredictionsTable = ({
+  loading,
+  error,
+  filenameStem,
+  data,
+  variantId,
+}: AbcPredictionsTableProps) => (
+  <OtTable
+    loading={loading}
+    error={error}
+    columns={tableColumns(variantId)}
+    data={data}
+    sortBy="pval"
+    order="asc"
+    downloadFileStem={filenameStem}
+  />
+);
+
+export default AbcPredictionsTable;
--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,11 @@
+if (!process.env.REACT_APP_BE_DOMAIN) {
+  var dotenv = require('dotenv');
+  var dotenvExpand = require('dotenv-expand');
+
+  const env = dotenv.config();
+  dotenvExpand.expand(env);
+}
+
 export function getApiUrl() {
   const domain = `http://${process.env.REACT_APP_BE_DOMAIN}`;
   const port = process.env.REACT_APP_BE_PORT;
