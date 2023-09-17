39b5accbf86328bd4490c2c26dbec5d022a4dab7
Riya Sinha <riya.v.sinha@gmail.com>
2023-09-17 03:45:45 -0700

    feat: update ingestion page to have queue all option and show stats

--- src/pages/IngestionPage/IngestionPage.tsx
+++ src/pages/IngestionPage/IngestionPage.tsx
@@ -1,22 +1,5 @@
-import {
-  Box,
-  Button,
-  Checkbox,
-  Container,
-  FormControlLabel,
-  Input,
-  Stack,
-  Typography,
-} from '@mui/material';
-import {
-  ChangeEvent,
-  MouseEvent,
-  useContext,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react';
+import { Box, Button, Stack, Typography } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
 import { SocketContext } from '../../socket';
 
 import Accordion from '@mui/material/Accordion';
@@ -28,7 +11,6 @@ import axios from 'axios';
 
 import BasePage from '../BasePage';
 import { getApiUrl } from '../../env';
-import { set } from 'react-ga';
 
 type File = {
   id: string;
@@ -38,10 +20,10 @@ type File = {
   log: string | null;
 };
 
-function UploadPage() {
+function IngestionPage() {
   const socket = useContext(SocketContext);
   const loading = useState(false);
-  const [files, setFiles] = useState<File[]>([]);
+  const [files, setFiles] = useState<File[] | null>([]);
   const [queue, setQueue] = useState<File[]>([]);
   const [expanded, setExpanded] = useState<string | false>(false);
 
@@ -50,7 +32,6 @@ function UploadPage() {
       setFiles(files);
     });
     socket.on('gcp-file-queue', (queue) => {
-      console.log(queue);
       setQueue(queue);
     });
   }, [socket]);
@@ -72,13 +53,22 @@ function UploadPage() {
       setExpanded(isExpanded ? panel : false);
     };
 
-  const processDataFile = (id: string, fileName: string) => async () => {
-    const response = await axios.post(`${getApiUrl()}/process-gcp-file`, {
-      id,
-      fileName,
+  const processDataFile = (file: File) => async () => {
+    const response = await axios.post(`${getApiUrl()}/process-file`, {
+      files: [file],
       operation: 'add',
     });
     setFiles(response.data);
+  };
+
+  const processAllFiles = async () => {
+    const filesToProcess = files?.filter(
+      (file) => file.status === 'Not Processed'
+    );
+    const response = await axios.post(`${getApiUrl()}/process-file`, {
+      files: filesToProcess,
+      operation: 'add',
+    });
   };
 
   return (
@@ -97,50 +87,70 @@ function UploadPage() {
         <Typography variant="h4" sx={{ marginBottom: '16px' }}>
           Files
         </Typography>
-        {files.map((file) => (
-          <Accordion
-            expanded={expanded === file.fileName}
-            onChange={handleChange(file.fileName)}
-            key={file.id}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1bh-content"
-              id="panel1bh-header"
+        <Typography variant="h6" sx={{ marginBottom: '8px' }}>
+          Number of Files: {files?.length}
+        </Typography>
+        <Typography variant="h6" sx={{ marginBottom: '8px' }}>
+          Not Processed:{' '}
+          {files?.filter((file) => file.status === 'Not Processed').length}
+        </Typography>
+        <Typography variant="h6" sx={{ marginBottom: '8px' }}>
+          Queued: {files?.filter((file) => file.status === 'Queued').length}
+        </Typography>
+        <Typography variant="h6" sx={{ marginBottom: '8px' }}>
+          Processing:{' '}
+          {files?.filter((file) => file.status === 'Processing').length}
+        </Typography>
+        <Typography variant="h6" sx={{ marginBottom: '8px' }}>
+          Processed:{' '}
+          {files?.filter((file) => file.status === 'Processed').length}
+        </Typography>
+        <Button variant="contained" onClick={processAllFiles}>
+          Queue All Unprocessed
+        </Button>
+        <Box sx={{ marginTop: '16px' }}>
+          {files?.map((file) => (
+            <Accordion
+              expanded={expanded === file.fileName}
+              onChange={handleChange(file.fileName)}
+              key={file.id}
             >
-              <Stack direction="row" width="100%">
-                <Typography sx={{ flex: 1 }}>{file.fileName}</Typography>
-                <Typography sx={{ flex: 1 }}>{file.status}</Typography>
-              </Stack>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Stack direction="row" width="100%">
-                <Button
-                  variant="contained"
-                  onClick={processDataFile(file.id, file.fileName)}
-                >
-                  Process Data
-                </Button>
-              </Stack>
-              <Box
-                sx={{
-                  marginTop: '16px',
-                  backgroundColor: '#eeeeee',
-                  height: '500px',
-                  overflow: 'auto',
-                  padding: '20px',
-                }}
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
               >
-                {file.log?.split('\n').map((line, index) => (
-                  <Typography key={index}>{line}</Typography>
-                ))}
-              </Box>
-            </AccordionDetails>
-          </Accordion>
-        ))}
+                <Stack direction="row" width="100%">
+                  <Typography sx={{ flex: 1 }}>{file.fileName}</Typography>
+                  <Typography sx={{ flex: 1 }}>{file.status}</Typography>
+                </Stack>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Stack direction="row" width="100%">
+                  <Button variant="contained" onClick={processDataFile(file)}>
+                    Process Data
+                  </Button>
+                </Stack>
+                <Box
+                  sx={{
+                    marginTop: '16px',
+                    backgroundColor: '#eeeeee',
+                    height: '500px',
+                    overflow: 'auto',
+                    padding: '20px',
+                  }}
+                >
+                  {file.log?.split('\n').map((line, index) => (
+                    <Typography key={index}>{line}</Typography>
+                  ))}
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </Box>
       </Box>
     </BasePage>
   );
 }
 
-export default UploadPage;
+export default IngestionPage;
