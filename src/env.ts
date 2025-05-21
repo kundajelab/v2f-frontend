b0f74171765efb7f8b42e804653f2abb952d6273
Riya Sinha <riya.v.sinha@gmail.com>
2025-05-21 01:40:20 -0700

    add https for nginx fe server, make sure to call https in backend

--- src/env.ts
+++ src/env.ts
@@ -8,7 +8,7 @@ if (!process.env.REACT_APP_BE_DOMAIN) {
 
 export function getApiUrl() {
   const domain = process.env.REACT_APP_BE_DOMAIN?.includes("$") ? process.env.BE_DOMAIN : process.env.REACT_APP_BE_DOMAIN;
-  const domainUrl = `http://${domain}`;
+  const domainUrl = `https://${domain}`;
   const port = process.env.REACT_APP_BE_PORT?.includes("$") ? process.env.BE_LOCAL_PORT : process.env.REACT_APP_BE_PORT;
   if (port) {
     return `${domainUrl}:${port}`;
